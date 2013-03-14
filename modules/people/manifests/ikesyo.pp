a8a6b54f3943a7b24f7d372f2b881865ca16f2b3
Syo Ikeda <suicaicoca@gmail.com>
2013-03-14 18:09:43 +0900

    常用アプリを追加

--- modules/people/manifests/ikesyo.pp
+++ modules/people/manifests/ikesyo.pp
@@ -26,6 +26,21 @@ class people::ikesyo {
     'Google 日本語入力':
       source   => "http://dl.google.com/japanese-ime/1.8.1310.1/googlejapaneseinput.dmg",
       provider => pkgdmg;
+    'CleanArchiver':
+      source   => "http://www.sopht.jp/pub/Mac/CleanArchiver-3.0a6.dmg",
+      provider => compressed_app;
+    'BetterTouchTool':
+      source   => "http://www.boastr.de/BetterTouchTool.zip",
+      provider => compressed_app;
+    'ClipMenu':
+      source   => "https://dl.dropbox.com/u/1140644/clipmenu/ClipMenu_0.4.3.dmg",
+      provider => compressed_app;
+    'CotEditor':
+      source   => "http://sourceforge.jp/frs/redir.php?m=osdn&f=%2Fcoteditor%2F54872%2FCotEditor_1.3.1_For10.7.dmg",
+      provider => compressed_app;
+    'SourceTree':
+      source   => "http://downloads.atlassian.com/software/sourcetree/SourceTree_1.5.7.1.dmg",
+      provider => compressed_app;
   }
 
   $home     = "/Users/${::luser}"
